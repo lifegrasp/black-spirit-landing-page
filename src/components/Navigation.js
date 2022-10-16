f47b917286eca32d3e91c597716d030e65cb929a
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2022-10-16 14:20:31 +0800

    finishing Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,22 +1,24 @@
 import { Link } from "react-router-dom";
-import '../styles/Navigation.css';
+import "../styles/Navigation.css";
 
 function Navigation() {
   return (
-      <nav className="Nav">
-        <div>Logo</div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/pricing">Pricing</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
+    <nav className="Nav">
+      <div className="logo">
+        <img alt="Black Spirit's Blessing" src="/images/logo.png" />
+      </div>
+      <ul>
+        <li className="navLink">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navLink">
+          <Link to="/pricing">Pricing</Link>
+        </li>
+        <li className="navLink">
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
