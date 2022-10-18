0c2139765429ac3a607e5fac15e3319e49ccc23a
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2022-10-18 17:08:18 +0800

    Home.js almost complete

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 import "../styles/Home.css";
 
 function Home() {
+  const enhanceDesc =
+    "No need to failstack to a certain stack. One tap is all you need to reach end game!";
+
+  const dropDesc =
+    "Spend less time hitting mobs for long hours just to get treasured items!";
+
+  const accuracyDesc =
+    "Sick of missing your targets because of their absurd evasion? With this, they'll be questioning if their evasion is even working at all!";
+
   return (
     <>
-      <h1>Services</h1>
+      <h1 className="services">Services</h1>
       <div className="showcase">
         <div className="video">
           <iframe
@@ -15,10 +24,42 @@ function Home() {
         </div>
         <div className="blessings">
           <div className="blessings-flex">
-            <div className="icon">ICON HERE</div>
+            <div className="icon">
+              <img
+                className="icon-image"
+                alt="enhancement icon"
+                src="images/icon-enhance.png"
+              />
+            </div>
             <div className="desc">
-              <h2>Blessing Title</h2>
-              <p>Blessing description here</p>
+              <h2>100% Enhancing Rate</h2>
+              <p>{enhanceDesc}</p>
+            </div>
+          </div>
+          <div className="blessings-flex">
+            <div className="icon">
+              <img
+                className="icon-image"
+                alt="drop rate icon"
+                src="images/icon-drop.png"
+              />
+            </div>
+            <div className="desc">
+              <h2>100% Drop Rate!</h2>
+              <p>{dropDesc}</p>
+            </div>
+          </div>
+          <div className="blessings-flex">
+            <div className="icon">
+              <img
+                className="icon-image"
+                alt="accuracy icon"
+                src="images/icon-accuracy.png"
+              />
+            </div>
+            <div className="desc">
+              <h2>100% Accuracy Rate!</h2>
+              <p>{accuracyDesc}</p>
             </div>
           </div>
         </div>
