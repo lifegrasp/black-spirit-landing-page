3e6cb71793a730a77b22cef24a4870b9fd8b2418
Henry Hart Alegrado <henryhartalegrado@gmail.com>
2022-10-20 11:43:12 +0800

    Footer is fully established

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,19 @@
+import "../styles/Footer.css";
+
 function Footer() {
+  const disclaimer =
+    "This project is made to display and hone my web development skills. None of the services offered here are real.";
   return (
     <>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
-      <h1>Footer</h1>
+      <footer>
+        <a
+          href="https://github.com/lifegrasp/black-spirit-landing-page"
+          target={"_blank"}
+        >
+          <img alt="GitHub logo" src="/images/GitHub_logo.png" />
+        </a>
+        <h3>{disclaimer}</h3>
+      </footer>
     </>
   );
 }
